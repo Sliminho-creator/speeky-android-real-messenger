411f9bb283b1366a172b2a4ec99dccabfa88c0f4
SUNTIX <arovojv96@gmail.com>
2026-03-02 20:29:57 +0400

    Fix Android CI build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
